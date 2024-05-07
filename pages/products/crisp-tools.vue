9553d5751247af37879fd99092e3be7ea8d9ee70
Rami James <ramijames@gmail.com>
2024-05-07 14:38:23 -0400

    Crisp tools product page is done

--- pages/products/crisp-tools.vue
+++ pages/products/crisp-tools.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="product-main">
     <section class="product-content">
+      <img src="/products/crisp-tools/crisp-logo-white.svg" alt="Crisp Tools Logo" class="product-logo">
       <h1 class="hyper-bold">Crisp Tools</h1>
-      <h4 class="centered">A simple, useful set of tools for designers and developers</h4>
+      <h4 class="centered">A simple, useful set of tools for designers and developers to work faster and be happier</h4>
       <img src="/products/crisp-tools/crisp-tools-home.png" alt="Crisp Tools Logo" class="product-image">
       <section class="product-bar">
         <section class="links">
@@ -13,28 +14,33 @@
         </section>
       </section>
     </section>
+    <section class="product-content-bg crisp-bg">
+      <section class="product-content">
+        <h2>Product Vision</h2>
+        <p>There are many types of small, critical tools which can speed up design or development workflows, which do not fall under a more general tool like Figma or Visual Studio.</p> 
+        <p>Tools that will be included in this product are:</p>
+        <ul>
+          <li><strong>PDF Tools</strong> - PDF join, PDF split</li>
+          <li><strong>Image Tools</strong> - Image compression, Image to text, Image to PDF, Image to base64, Image to ASCII, Image to URL</li>
+          <li><strong>Color Tools</strong> - Palette Generator, Lighten, Darken, RGB/HEX converter, Contrast tools</li>
+          <li><strong>Converter Tools</strong> - Number base, YAML</li>
+          <li><strong>Time and Date</strong> - Unix Timestamp Converter, Lunar cycle, Days/month left in the year, Time / location conversion</li>
+          <li><strong>Encoders/decoders</strong> - JWT, Base64</li>
+          <li><strong>Formatters</strong> - JSON, XML, SQL, HTML, Markdown</li>
+          <li><strong>Generator Tools</strong> - Hashing, UUID</li>
+          <li><strong>Text Tools</strong> - Typography, Lorem Ipsum</li>
+          <li><strong>Icon Tools</strong> - Favicon, iOS, Android, Windows</li>
+        </ul>
+      </section>
+    </section>
     <section class="product-content">
-      <h2>Product Vision</h2>
-      <p>There are many types of small, critical tools which can speed up design or development workflows, which do not fall under a more general tool like Figma or Visual Studio.</p> 
-      <p>Tools that will be included in this product are:</p>
-      <ul>
-        <li><strong>PDF Tools</strong> - PDF join, PDF split</li>
-        <li><strong>Image Tools</strong> - Image compression, Image to text, Image to PDF, Image to base64, Image to ASCII, Image to URL</li>
-        <li><strong>Color Tools</strong> - Palette Generator, Lighten, Darken, RGB/HEX converter, Contrast tools</li>
-        <li><strong>Converter Tools</strong> - Number base, YAML</li>
-        <li><strong>Time and Date</strong> - Unix Timestamp Converter, Lunar cycle, Days/month left in the year, Time / location conversion</li>
-        <li><strong>Encoders/decoders</strong> - JWT, Base64</li>
-        <li><strong>Formatters</strong> - JSON, XML, SQL, HTML, Markdown</li>
-        <li><strong>Generator Tools</strong> - Hashing, UUID</li>
-        <li><strong>Text Tools</strong> - Typography, Lorem Ipsum</li>
-        <li><strong>Icon Tools</strong> - Favicon, iOS, Android, Windows</li>
-      </ul>
-
       <h2>Brand</h2>
-      <p>The Crisptools brand concept is intended to be simple and clean. The logo is a "crisp", or potato chip. Small, bite-sized bits that taste good on their own.</p>
+      <p class="centered">The Crisptools brand concept is intended to be simple and clean. The logo is a "crisp", or potato chip. Small, bite-sized bits that taste good on their own.</p>
       <img src="/products/crisp-tools/crisp-logo-brand.svg" alt="Crisp Tools Logo" class="product-image">
-      <p>The associated iconography is customized to reflect the simple, bright nature of the brand. Icons are fun, clear, and easy to understand.</p>
+      <p class="centered">The associated iconography is customized to reflect the simple, bright nature of the brand. Icons are fun, clear, and easy to understand.</p>
       <img src="/products/crisp-tools/crisp-icons-brand.svg" alt="Crisp Tools Icons" class="product-image">
+
+      <hr>
 
       <h2>Interface Design</h2>
       <p>The interface design is intended to be simple and clean. The color palette is bright and fun, with a focus on usability and clarity.</p>
