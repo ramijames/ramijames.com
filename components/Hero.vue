35aa3a7cf2fd5267f26089b88d9c70d98e666dbc
Rami James <ramijames@gmail.com>
2024-05-11 18:07:49 -0400

    Alternate homepage with components

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,24 @@
 <template>
-  <main>
-    <Woosh :numberOfElements="10" :size="40" color="#ff9900" :borderWidth="1" animationStyle="default" />
+  <main class="hero-for-the-zero">
+    <section class=""></section>
+    <Woosh :numberOfElements="numberOfElements" :size="40" color="#ff9900" :borderWidth="4" animationStyle="defaultWoosh" />
+    <section class="animPanel">
+      <!-- A panel to control the props that are input into the Woosh component -->
+      <label for="numberOfElements">Number of Elements</label>
+      <input type="number" id="numberOfElements" name="numberOfElements" min="1" max="100" v-model="numberOfElements" />
+      <!-- <label for="size">Size</label> -->
+      <!-- <input type="range" id="size" name="size" min="1" max="100" v-model="size" />
+      <label for="color">Color</label>
+      <input type="color" id="color" name="color" v-model="color" />
+      <label for="borderWidth">Border Width</label>
+      <input type="range" id="borderWidth" name="borderWidth" min="1" max="100" v-model="borderWidth" />
+      <label for="animationStyle">Animation Style</label>
+      <input type="text" id="animationStyle" name="animationStyle" v-model="animationStyle" /> -->
+    </section>
   </main>
+  <section class="services-sell">
+    other stuff
+  </section>
 </template>
 
 <script>
@@ -9,9 +26,64 @@
 import Woosh from './Woosh.vue'
 
 export default {
+  data(){
+    return {
+      title: 'Home',
+      numberOfElements: 10,
+      size: 40,
+      color: '#ff9900',
+      borderWidth: 10,
+      animationStyle: 'defaultWoosh'
+    }
+  },
+  setup() {
+    
+    
+    return {
+      
+    }
+  },
   components: {
     Woosh
   }
 }
 
 </script>
+
+<style scoped>
+
+.hero-for-the-zero {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+
+    .animPanel {
+      position: absolute;
+      bottom: 2rem;
+      right: 2rem;
+      background-color: rgba(89, 48, 103, 0.1);
+      backdrop-filter: blur(5px);
+      padding: 20px;
+      border: 1px solid rgba(255, 255, 255, 0.371);
+      border-radius:.3rem;
+      z-index: 1000;
+      box-shadow: 0 18px 32px 0 rgba(25, 27, 50, 0.164);
+    }
+
+.services-sell {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0f0f0;
+}
+
+</style>
--- components/Woosh.vue
+++ components/Woosh.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="container">
-    <div
-      v-for="i in numberOfElements"
-      :key="i"
-      :style="{
-        width: size + '%',
-        height: size + '%',
-        backgroundColor: color,
-        borderWidth: borderWidth + 'px',
-        animation: animationStyle + ' 2s infinite'
-      }"
-      class="element"
-    ></div>
+  <div class="animBox">
+    <div class="rotateGroup">
+      <div
+        v-for="(element, index) in numberOfElements"
+          :key="element"
+          :style="{
+            // width: size + '%',
+            // height: size + '%',
+            // height: '200px',
+            // backgroundColor: color,
+            borderWidth: borderWidth + 'px',
+            animation: animationStyle + ' ' + (index + 1) * 3 + 's' + ' infinite',
+            // top: index * 10 + 'px',
+          }"
+          class="element"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -42,9 +46,31 @@ export default {
 }
 </script>
 
-<style scoped>
-.container {
-  perspective: 1000px;
+<style>
+.animBox {
+  perspective: 600em;
+  position:absolute;
+  right:0;
+  top:0;
+  bottom:0;
+  left:0;
+  z-index: 10;
+  height: 100vh;
+  width: 100vw;
+  display:flex;
+  justify-content:center;
+  align-content: center;
+  align-items: center;
+  pointer-events: none; /* Can't click on it */
+}
+
+.rotateGroup {
+  transform-style: preserve-3d;
+  transform: translateX(130px) rotate3d(2.5,1.75,6,90deg);
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 .element {
@@ -52,5 +78,62 @@ export default {
   border-style: solid;
   border-color: black;
   transform-style: preserve-3d;
+  width:400px;
+  height:400px;
+  border-radius: 50%;
+  /* scale: 0.1; */
+  transform:  translateY(0)
+              rotate3d(1, 0, 0, 90deg);
+  animation-iteration-count: 1;
 }
+
+@keyframes defaultWoosh {
+
+  /* animation steps
+
+  1. start state
+  2. grow to full size, set at bottom
+  3. stay at full size, move to top
+  4. stay at full size, stay at top
+  5. shrink to start size, move to bottom
+
+  */
+
+  0% {
+    width:80px;
+    height:80px;
+    transform:  translateZ(0px)
+                rotate3d(1, 0, 0, 0deg);
+  }
+
+  20% {
+    width:800px;
+    height:800px;
+    transform:  translateZ(0px)
+                rotate3d(1, 0, 0, 0deg);
+  }
+
+  40% {
+    width:800px;
+    height:800px;
+    transform:  translateZ(1200px)
+                rotate3d(1, 0, 0, 0deg);
+  }
+
+  60% {
+    width:80px;
+    height:80px;
+    transform:  translateZ(1200px)
+                rotate3d(1, 0, 0, 0deg);
+  }
+
+  100% {
+    width:80px;
+    height:80px;
+    transform:  translateZ(0px)
+                rotate3d(1, 0, 0, 0deg);
+  }
+  
+}
+
 </style>
