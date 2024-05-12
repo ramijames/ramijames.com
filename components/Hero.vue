97dedccccb8ef6986e1a5eed4e97d3308fc93748
Rami James <ramijames@gmail.com>
2024-05-12 10:38:16 -0400

    Making big changes, committing first

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,11 @@
 <template>
   <main class="hero-for-the-zero">
-    <section class=""></section>
-    <Woosh :numberOfElements="numberOfElements" :size="40" color="#ff9900" :borderWidth="100" animationStyle="defaultWoosh" />
+    <section class="hero-text">
+      <h1 class="home-bold">Fast-track your project</h1>
+      <h4>Subscription access for startups to one of the world's best UI/UX experts</h4>
+      <nuxt-link to="/subscribe" class="button">Subscribe</nuxt-link>
+    </section>
+    <Woosh :numberOfElements="numberOfElements" :size="40" color="#ff9900" :borderWidth="10" animationStyle="defaultWoosh" />
     <section class="animPanel">
       <!-- A panel to control the props that are input into the Woosh component -->
       <label for="numberOfElements">Number of Elements</label>
@@ -62,19 +66,25 @@ export default {
   overflow:hidden;
 }
 
-
+    .hero-text {
+      text-align:center;      
+    }
 
     .animPanel {
       position: absolute;
       bottom: 2rem;
       right: 2rem;
       background-color: rgba(89, 48, 103, 0.1);
-      backdrop-filter: blur(5px);
+      backdrop-filter: blur(16px);
       padding: 20px;
-      border: 1px solid rgba(255, 255, 255, 0.371);
       border-radius:.3rem;
       z-index: 1000;
       box-shadow: 0 18px 32px 0 rgba(25, 27, 50, 0.164);
+    }
+
+    .dark .animPanel {
+      background-color: #15071d6e;
+      box-shadow: 0 18px 32px 0 rgba(54, 11, 47, 0.28);
     }
 
 .services-sell {
