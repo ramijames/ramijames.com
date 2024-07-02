0275cf996168b0fe575bcfadc06005e93de6f78a
Rami James <ramijames@gmail.com>
2024-07-02 11:24:36 -0400

    Small icons for services

--- pages/services.vue
+++ pages/services.vue
@@ -6,6 +6,19 @@
     <section id="QuickServices">
       <section id="ServicesGrid">
         <section class="single-service">
+          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M32.0538 68.2397C32.0538 63.6984 29.5885 54.6157 19.7274 54.6157C7.40091 54.6157 2.21083 68.8884 6.75215 76.6736C11.2935 84.4587 15.186 89 25.5662 89C33.8703 89 58.8692 89 71.6282 89C82.6571 89 91.7398 83.8099 91.7398 74.7273C91.7398 63.6984 84.9256 61.1033 84.9256 61.1033" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M78.7646 66.2934C82.4409 64.9959 88.496 59.676 83.3059 48.7768C78.7646 39.2401 69.6819 40.9917 69.6819 40.9917" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M63.8431 68.8884C57.1392 69.5372 45.4183 74.4678 52.1654 89" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M59.3018 54.6157C56.2742 57.6433 51.1274 65.1256 54.7604 70.8347" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M46.3265 53.967C48.7053 53.3182 53.8522 53.3182 55.4092 58.5083" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M26.215 39.6942C29.0263 34.5041 37.5034 26.5893 48.9216 36.4504C57.3555 30.6116 76.8183 36.4504 67.7356 52.0207" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M28.4834 57.2107C29.9972 55.2645 36.2686 52.0207 41.1364 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M37.2439 45.5331C32.0538 36.4504 10.2555 38.5264 11.2935 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M12.2644 25.4215L7.72311 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M89.4669 25.4215L94.0083 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M50.5413 20.2314V10.5" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+          </svg>
           <h4>Product</h4>
           <ul>
             <li>Concept</li>
@@ -15,6 +28,13 @@
           </ul>
         </section>
         <section class="single-service">
+          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.5914 31.0464C-14.4508 84.3277 52.1508 106.173 62.2743 90.1887C64.9383 82.7293 45.1324 81.6636 53.2164 69.9418C61.3005 58.2199 86.2508 78.4668 92.6446 61.9496C108.564 20.8251 36.9438 -25.0226 10.5914 31.0464Z" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <circle cx="73.9961" cy="48.0965" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <circle cx="55.8805" cy="25.7183" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <circle cx="30.3055" cy="36.3745" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <circle cx="27.1086" cy="65.1465" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+          </svg>
           <h4>Design</h4>
           <ul>
             <li>Wireframing</li>
@@ -24,6 +44,11 @@
           </ul>
         </section>
         <section class="single-service">
+          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="5" y="5" width="90" height="90" rx="10" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M5 24L9.62344 24C12.3362 24 14.5541 21.8369 14.6219 19.125L14.6281 18.875C14.6959 16.1631 16.9138 14 19.6266 14L47.5 14C50.2614 14 52.5 16.2386 52.5 19V19C52.5 21.7614 54.7386 24 57.5 24L95 24" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            <path d="M5 86L95 86" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+          </svg>
           <h4>Development</h4>
           <ul>
             <li>Front-end Development</li>
@@ -135,6 +160,11 @@ import Button from '/components/Button';
         margin-bottom: $spacing-md;
       }
 
+      svg {
+        width: 60px;
+        margin-bottom: $spacing-md;
+      }
+
       h4 {
         margin-top: 0;
         margin-bottom: $spacing-xs;
@@ -159,6 +189,12 @@ import Button from '/components/Button';
   #ServicesGrid {
     .single-service {
       border-left: 2px solid $white;
+
+      svg {
+        path, circle, rect {
+          stroke: $white;
+        }
+      }
     }
   }
 }
