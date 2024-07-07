4f03bd2eab38334922ae5aff65a35329cfd3e125
Rami James <ramijames@gmail.com>
2024-07-07 08:33:32 -0400

    Simpler theme switcher

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="theme-switcher-box" @click="toggleTheme">
-    <div class="theme-switcher-sun"
+    <div class="theme-switcher"
       :class="`${currentTheme}`"
     >
+      <img :src="`${currentTheme}.svg`" alt="theme icon" />
     </div>
   </div>
 </template>
@@ -50,19 +51,6 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  transform: scale(0.8);
-  mix-blend-mode: luminosity;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    transform: scale(1);
-    mix-blend-mode: initial;
-  }
-
-  @media screen and (max-width: 768px){
-    right: 0;
-    top: 0;
-  }
 
   span {
     background: $white-dark;
@@ -73,73 +61,38 @@ export default {
     font-weight: 500;
     color: rgba($black,0.4);
   }
+
+  .theme-switcher {
+    position: relative;
+    width: 60px;
+    border: 2px solid $black;
+    border-radius: 30px;
+    height: 32px;
+
+    img {
+      cursor: pointer;
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+      margin: 2px;
+      position: absolute;
+      transition: all 0.3s ease-in-out;
+    }
+
+    &.light img {
+      left: 0;
+    }
+
+    &.dark img {
+      left: 24px;
+    }
+  }
 }
 
 .dark .theme-switcher-box {
 
-  span {
-    background: $black-light;
-    color: rgba($white,0.4);
-  }
-
-  &::after {
-    background: rgba($blue, 0.8);
-
-    @media screen and (max-width: 768px){
-      background: rgba($white, 0.8);
-    }
-  }
-
-  &::before {
-    background: rgba($blue, 0.8);
-
-    @media screen and (max-width: 768px){
-      background: rgba($white, 0.8);
-    }
-  }
-}
-
-img {
-  cursor: pointer;
-  width: 32px;
-  height: 32px;
-}
-
-.theme-switcher-sun { /* MOON */
-  width:32px;
-  height:32px;
-  border-radius: 50%;
-  background-color: rgb(140, 228, 250);
-  transition: all 0.3s ease-in-out;
-  overflow: hidden;
-  position: relative;
-  outline: 2px solid rgba($black, 0.4);
-  outline-offset: 2px;
-  margin: 4px 0;
-
-  &:after { /* MOON */
-    content: '';
-    position: absolute;
-    top: -8px;
-    right: -8px;
-    width:32px;
-    height:32px;
-    background: linear-gradient(45deg, rgba($blue, 0.8), rgba($black, 0.2));
-    transition: all 0.24s ease-in-out;
-    border-radius: 50%;
-    box-shadow: inset 0 0 12px rgba($blue, 0.4), 0 0 10px 0 rgba($white, 0.5);
-  }
-
-  &.light { /* SUN */
-    outline: 2px solid rgba($orange, 0.4);
-    border: 2px solid rgba($orange, 1);
-
-    &:after { /* SUN */
-      background: $orange-light;
-      right: 0px;
-      top:0;
-      box-shadow: inset 0 0 8px rgba($orange-dark, 0.2);
-    }
+  .theme-switcher {
+    border: 2px solid $white;
   }
 }
 
