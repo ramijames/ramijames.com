b6b11423777f8aec317a785ffd29a047118a2727
Rami James <ramijames@gmail.com>
2024-09-27 12:47:30 +0300

    More css witchery

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,64 +1,67 @@
 <template>
-  <section class="products-hero">
-    <section class="intro-text w-three-quarters">
-      <p class="small blue">
-        Love for products
-      </p>
-      <p>
-        <span>R</span>
-        <span>e</span>
-        <span>c</span>
-        <span>e</span>
-        <span>n</span>
-        <span>t</span>
-        <span>&nbsp;</span>
-        <span>W</span>
-        <span>o</span>
-        <span>r</span>
-        <span>k</span>
-      </p>
-      <section class="products-buttons">
-        <nuxt-link class="button large" to="/services">What can I do for you?</nuxt-link>
-        <a class="button large" href="mailto:ramijames@gmail.com">Say hello!</a>
-        <div class="arrow-down">
-          <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M22 42C33.0457 42 42 33.0457 42 22C42 10.9543 33.0457 2 22 2C10.9543 2 2 10.9543 2 22C2 33.0457 10.9543 42 22 42ZM22 44C34.1503 44 44 34.1503 44 22C44 9.84974 34.1503 0 22 0C9.84974 0 0 9.84974 0 22C0 34.1503 9.84974 44 22 44Z" fill="white" fill-opacity="0.2"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M21 30.5858V10H23V30.5858L27.6569 25.9289C28.0474 25.5384 28.6806 25.5384 29.0711 25.9289C29.4616 26.3195 29.4616 26.9526 29.0711 27.3431L22.7071 33.7071C22.3166 34.0976 21.6834 34.0976 21.2929 33.7071L14.9289 27.3431C14.5384 26.9526 14.5384 26.3195 14.9289 25.9289C15.3195 25.5384 15.9526 25.5384 16.3432 25.9289L21 30.5858Z" fill="white"/>
-          </svg>
-        </div>
+  <main id="ProjectsMain">
+    <div class="mask">
+      <div id="mesh-gradient" class="sixteen-elements">
+        <div class="element" v-for="n in 160" :key="n"></div>
+      </div>
+    </div>
+    <section class="products-hero">
+      <section class="intro-text w-three-quarters">
+        <p class="small blue">
+          Love for products
+        </p>
+        <p>
+          <span>R</span>
+          <span>e</span>
+          <span>c</span>
+          <span>e</span>
+          <span>n</span>
+          <span>t</span>
+          <span>&nbsp;</span>
+          <span>W</span>
+          <span>o</span>
+          <span>r</span>
+          <span>k</span>
+        </p>
+        <section class="products-buttons">
+          <nuxt-link class="button large" to="/services">What can I do for you?</nuxt-link>
+          <a class="button large" href="mailto:ramijames@gmail.com">Say hello!</a>
+          <div class="arrow-down">
+            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M22 42C33.0457 42 42 33.0457 42 22C42 10.9543 33.0457 2 22 2C10.9543 2 2 10.9543 2 22C2 33.0457 10.9543 42 22 42ZM22 44C34.1503 44 44 34.1503 44 22C44 9.84974 34.1503 0 22 0C9.84974 0 0 9.84974 0 22C0 34.1503 9.84974 44 22 44Z" fill="white" fill-opacity="0.2"/>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M21 30.5858V10H23V30.5858L27.6569 25.9289C28.0474 25.5384 28.6806 25.5384 29.0711 25.9289C29.4616 26.3195 29.4616 26.9526 29.0711 27.3431L22.7071 33.7071C22.3166 34.0976 21.6834 34.0976 21.2929 33.7071L14.9289 27.3431C14.5384 26.9526 14.5384 26.3195 14.9289 25.9289C15.3195 25.5384 15.9526 25.5384 16.3432 25.9289L21 30.5858Z" fill="white"/>
+            </svg>
+          </div>
+        </section>
       </section>
     </section>
-  </section>
-  <section id="projects" class="w-full" ref="projects">
-    <section id="company-logos">
-      <img src="/social-proof/wix.png" alt="Wix" />
-      <img src="/social-proof/ibm.png" alt="IBM" />
-      <img src="/social-proof/spiral.png" alt="Spiral" />
-      <img src="/social-proof/wpt.png" alt="WPT" />
-      <img src="/social-proof/qmarkets.png" alt="Qmarkets" />
-      <img src="/social-proof/telos.png" alt="Telos" />
-      <img src="/social-proof/ultra.png" alt="Ultra" />
+    <section id="projects" class="w-full" ref="projects">
+      <section id="company-logos">
+        <img src="/social-proof/wix.png" alt="Wix" />
+        <img src="/social-proof/ibm.png" alt="IBM" />
+        <img src="/social-proof/spiral.png" alt="Spiral" />
+        <img src="/social-proof/wpt.png" alt="WPT" />
+        <img src="/social-proof/qmarkets.png" alt="Qmarkets" />
+        <img src="/social-proof/telos.png" alt="Telos" />
+        <img src="/social-proof/ultra.png" alt="Ultra" />
+      </section>
     </section>
-  </section>
-  <!-- <section id="projects" class="w-three-quarters page-top" ref="projects">
-    <h1 class="">Recent Work</h1>
-    <h4 class="">I'm proud to have worked on these products over the years</h4>
-    <section id="company-logos">
-      <img src="/social-proof/wix.png" alt="Wix" />
-      <img src="/social-proof/ibm.png" alt="IBM" />
-      <img src="/social-proof/spiral.png" alt="Spiral" />
-      <img src="/social-proof/wpt.png" alt="WPT" />
-      <img src="/social-proof/qmarkets.png" alt="Qmarkets" />
-      <img src="/social-proof/telos.png" alt="Telos" />
-      <img src="/social-proof/ultra.png" alt="Ultra" />
-    </section>
-  </section> -->
+  </main> 
   <MergedProjects />
 </template>
 
 <style scoped lang="scss">
 
 @import './assets/variables';
+
+#ProjectsMain {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  perspective: 100rem;
+}
 
 .products-hero {
   padding: $spacing-lg 0 0 0;
@@ -170,6 +173,93 @@
     width: 80px;
     opacity: 0.5;
     filter: invert(1);
+  }
+}
+
+.mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0,0,0,0.5);
+  z-index: -1;
+  mask-image: linear-gradient( rgba(0,0,0,0) 5%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 80%);
+}
+
+#mesh-gradient.sixteen-elements {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  z-index: -10;
+  align-items: center;
+  justify-content: center;;
+  opacity: 1;
+  position: fixed;
+  pointer-events: none;
+  top:0;
+  left:0;
+  right:0;
+  animation: rotateGroup 120s linear infinite;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+
+  $mesh-colors: ();
+
+  // generate the colors with an HSL model
+  @for $i from 0 through 360 {
+    $hue: ($i - 1) * calc(360 / 60);
+    $color: hsl($hue, 100%, 50%);
+    $mesh-colors: map-merge($mesh-colors, ($i: $color));
+  }
+
+  $i: 0;
+  @each $name, $color in $mesh-colors {
+    $i: $i + 1;
+    .element:nth-child(#{$i}) {
+      // background: linear-gradient(to bottom, rgba($color,0) 55%, rgba($color,.8));
+      border: 1px solid rgba($color, .4);
+      animation-delay: -#{$i * .15}s;
+      width: 1px;
+      height: 10dvw;
+      border-radius: 5dvw;
+      align-self: center;
+      justify-self: center;
+    }
+  }
+
+  .element {
+    animation: sixteen-elements 10s linear infinite;
+  }
+
+  @keyframes rotateGroup {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes sixteen-elements {
+    0% {
+      opacity:0;
+      transform: scale(0) rotate(0deg);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.6) rotate(180deg);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(0) rotate(360deg);
+    }
   }
 }
 
--- pages/services.vue
+++ pages/services.vue
@@ -1,116 +1,123 @@
 <template>
-  <section class="services-hero">
-    <section class="intro-text w-three-quarters">
-      <p class="small blue">
-        Why we're here
-      </p>
-      <p>
-        <span>S</span>
-        <span>e</span>
-        <span>r</span>
-        <span>v</span>
-        <span>i</span>
-        <span>c</span>
-        <span>e</span>
-        <span>s</span>
-      </p>
-      <section class="services-buttons">
-        <nuxt-link class="button large" to="/products">Seen the recent work?</nuxt-link>
-        <a class="button large" href="mailto:ramijames@gmail.com">Say hello!</a>
-        <div class="arrow-down">
-          <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M22 42C33.0457 42 42 33.0457 42 22C42 10.9543 33.0457 2 22 2C10.9543 2 2 10.9543 2 22C2 33.0457 10.9543 42 22 42ZM22 44C34.1503 44 44 34.1503 44 22C44 9.84974 34.1503 0 22 0C9.84974 0 0 9.84974 0 22C0 34.1503 9.84974 44 22 44Z" fill="white" fill-opacity="0.2"/>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M21 30.5858V10H23V30.5858L27.6569 25.9289C28.0474 25.5384 28.6806 25.5384 29.0711 25.9289C29.4616 26.3195 29.4616 26.9526 29.0711 27.3431L22.7071 33.7071C22.3166 34.0976 21.6834 34.0976 21.2929 33.7071L14.9289 27.3431C14.5384 26.9526 14.5384 26.3195 14.9289 25.9289C15.3195 25.5384 15.9526 25.5384 16.3432 25.9289L21 30.5858Z" fill="white"/>
-          </svg>
-        </div>
+  <main id="Services">
+    <div class="mask">
+      <div id="mesh-gradient" class="sixteen-elements">
+        <div class="element" v-for="n in 160" :key="n"></div>
+      </div>
+    </div>
+    <section class="services-hero">
+      <section class="intro-text w-three-quarters">
+        <p class="small blue">
+          Why we're here
+        </p>
+        <p>
+          <span>S</span>
+          <span>e</span>
+          <span>r</span>
+          <span>v</span>
+          <span>i</span>
+          <span>c</span>
+          <span>e</span>
+          <span>s</span>
+        </p>
+        <section class="services-buttons">
+          <nuxt-link class="button large" to="/products">Seen the recent work?</nuxt-link>
+          <a class="button large" href="mailto:ramijames@gmail.com">Say hello!</a>
+          <div class="arrow-down">
+            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M22 42C33.0457 42 42 33.0457 42 22C42 10.9543 33.0457 2 22 2C10.9543 2 2 10.9543 2 22C2 33.0457 10.9543 42 22 42ZM22 44C34.1503 44 44 34.1503 44 22C44 9.84974 34.1503 0 22 0C9.84974 0 0 9.84974 0 22C0 34.1503 9.84974 44 22 44Z" fill="white" fill-opacity="0.2"/>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M21 30.5858V10H23V30.5858L27.6569 25.9289C28.0474 25.5384 28.6806 25.5384 29.0711 25.9289C29.4616 26.3195 29.4616 26.9526 29.0711 27.3431L22.7071 33.7071C22.3166 34.0976 21.6834 34.0976 21.2929 33.7071L14.9289 27.3431C14.5384 26.9526 14.5384 26.3195 14.9289 25.9289C15.3195 25.5384 15.9526 25.5384 16.3432 25.9289L21 30.5858Z" fill="white"/>
+            </svg>
+          </div>
+        </section>
       </section>
     </section>
-  </section>
-  <section class="w-half">
-    <section id="QuickServices">
-      <section id="ServicesGrid">
-        <section class="single-service product">
-          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M32.0538 68.2397C32.0538 63.6984 29.5885 54.6157 19.7274 54.6157C7.40091 54.6157 2.21083 68.8884 6.75215 76.6736C11.2935 84.4587 15.186 89 25.5662 89C33.8703 89 58.8692 89 71.6282 89C82.6571 89 91.7398 83.8099 91.7398 74.7273C91.7398 63.6984 84.9256 61.1033 84.9256 61.1033" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M78.7646 66.2934C82.4409 64.9959 88.496 59.676 83.3059 48.7768C78.7646 39.2401 69.6819 40.9917 69.6819 40.9917" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M63.8431 68.8884C57.1392 69.5372 45.4183 74.4678 52.1654 89" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M59.3018 54.6157C56.2742 57.6433 51.1274 65.1256 54.7604 70.8347" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M46.3265 53.967C48.7053 53.3182 53.8522 53.3182 55.4092 58.5083" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M26.215 39.6942C29.0263 34.5041 37.5034 26.5893 48.9216 36.4504C57.3555 30.6116 76.8183 36.4504 67.7356 52.0207" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M28.4834 57.2107C29.9972 55.2645 36.2686 52.0207 41.1364 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M37.2439 45.5331C32.0538 36.4504 10.2555 38.5264 11.2935 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M12.2644 25.4215L7.72311 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M89.4669 25.4215L94.0083 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M50.5413 20.2314V10.5" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-          </svg>
-          <h2>Product</h2>
-          <ul>
-            <li>Concept</li>
-            <li>Vision</li>
-            <li>Roadmapping</li>
-            <li>Leadership</li>
-          </ul>
-          <p>Ensuring that you are building the right things for the right reasons can be a monumental task. I have decades of experience helping teams make smart decisions about their product, ensuring that it matches both business and user needs.</p>
+    <section class="w-half">
+      <section id="QuickServices">
+        <section id="ServicesGrid">
+          <section class="single-service product">
+            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M32.0538 68.2397C32.0538 63.6984 29.5885 54.6157 19.7274 54.6157C7.40091 54.6157 2.21083 68.8884 6.75215 76.6736C11.2935 84.4587 15.186 89 25.5662 89C33.8703 89 58.8692 89 71.6282 89C82.6571 89 91.7398 83.8099 91.7398 74.7273C91.7398 63.6984 84.9256 61.1033 84.9256 61.1033" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M78.7646 66.2934C82.4409 64.9959 88.496 59.676 83.3059 48.7768C78.7646 39.2401 69.6819 40.9917 69.6819 40.9917" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M63.8431 68.8884C57.1392 69.5372 45.4183 74.4678 52.1654 89" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M59.3018 54.6157C56.2742 57.6433 51.1274 65.1256 54.7604 70.8347" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M46.3265 53.967C48.7053 53.3182 53.8522 53.3182 55.4092 58.5083" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M26.215 39.6942C29.0263 34.5041 37.5034 26.5893 48.9216 36.4504C57.3555 30.6116 76.8183 36.4504 67.7356 52.0207" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M28.4834 57.2107C29.9972 55.2645 36.2686 52.0207 41.1364 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M37.2439 45.5331C32.0538 36.4504 10.2555 38.5264 11.2935 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M12.2644 25.4215L7.72311 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M89.4669 25.4215L94.0083 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M50.5413 20.2314V10.5" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+            <h2>Product</h2>
+            <ul>
+              <li>Concept</li>
+              <li>Vision</li>
+              <li>Roadmapping</li>
+              <li>Leadership</li>
+            </ul>
+            <p>Ensuring that you are building the right things for the right reasons can be a monumental task. I have decades of experience helping teams make smart decisions about their product, ensuring that it matches both business and user needs.</p>
+          </section>
+          <section class="single-service design">
+            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M10.5914 31.0464C-14.4508 84.3277 52.1508 106.173 62.2743 90.1887C64.9383 82.7293 45.1324 81.6636 53.2164 69.9418C61.3005 58.2199 86.2508 78.4668 92.6446 61.9496C108.564 20.8251 36.9438 -25.0226 10.5914 31.0464Z" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <circle cx="73.9961" cy="48.0965" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <circle cx="55.8805" cy="25.7183" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <circle cx="30.3055" cy="36.3745" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <circle cx="27.1086" cy="65.1465" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+            <h2>Design</h2>
+            <ul>
+              <li>Wireframing</li>
+              <li>Prototyping</li>
+              <li>Mockups</li>
+              <li>Illustrations</li>
+            </ul>
+            <p>From user experience wireframes and prototypes to high-fidelity mockups, I excel at making simple looking, highly usable interfaces that stand out in our shared competitive market.</p>
+          </section>
+          <section class="single-service development">
+            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="5" y="5" width="90" height="90" rx="10" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M5 24L9.62344 24C12.3362 24 14.5541 21.8369 14.6219 19.125L14.6281 18.875C14.6959 16.1631 16.9138 14 19.6266 14L47.5 14C50.2614 14 52.5 16.2386 52.5 19V19C52.5 21.7614 54.7386 24 57.5 24L95 24" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <path d="M5 86L95 86" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+            <h2>Development</h2>
+            <ul>
+              <li>Front-end Development</li>
+              <li>Vue/Nuxt</li>
+              <li>Accessibility</li>
+              <li>Web3</li>
+            </ul>
+            <p>I have decades of experience building software used by hundreds of thousands of users around the world. I focus on creating fast, easy-to-use, and accessible frontends in modern JavaScript frameworks.</p>
+          </section>
+          <!-- <section class="single-service analysis">
+            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 5V85C5 90.5228 9.47715 95 15 95H95" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <rect x="15" y="59" width="12" height="26" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <rect x="35" y="20" width="12" height="65" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <rect x="55" y="45" width="12" height="40" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+              <rect x="75" y="13" width="12" height="72" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
+            </svg>
+            <h2>Analysis</h2>
+            <ul>
+              <li>Product Success</li>
+              <li>Team Process</li>
+              <li>User Journey</li>
+              <li>Metrics Definition</li>
+            </ul>
+            <p>An extension of the product cycle is having smart analysis of the product that you have built. This means setting up analytics tools like Hotjar, and monitoring them with an intent to feed that output back into your product cycle. It's critical to iterate based on hard data and not intuition.</p>
+          </section> -->
         </section>
-        <section class="single-service design">
-          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.5914 31.0464C-14.4508 84.3277 52.1508 106.173 62.2743 90.1887C64.9383 82.7293 45.1324 81.6636 53.2164 69.9418C61.3005 58.2199 86.2508 78.4668 92.6446 61.9496C108.564 20.8251 36.9438 -25.0226 10.5914 31.0464Z" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <circle cx="73.9961" cy="48.0965" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <circle cx="55.8805" cy="25.7183" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <circle cx="30.3055" cy="36.3745" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <circle cx="27.1086" cy="65.1465" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-          </svg>
-          <h2>Design</h2>
-          <ul>
-            <li>Wireframing</li>
-            <li>Prototyping</li>
-            <li>Mockups</li>
-            <li>Illustrations</li>
-          </ul>
-          <p>From user experience wireframes and prototypes to high-fidelity mockups, I excel at making simple looking, highly usable interfaces that stand out in our shared competitive market.</p>
-        </section>
-        <section class="single-service development">
-          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="5" y="5" width="90" height="90" rx="10" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M5 24L9.62344 24C12.3362 24 14.5541 21.8369 14.6219 19.125L14.6281 18.875C14.6959 16.1631 16.9138 14 19.6266 14L47.5 14C50.2614 14 52.5 16.2386 52.5 19V19C52.5 21.7614 54.7386 24 57.5 24L95 24" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <path d="M5 86L95 86" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-          </svg>
-          <h2>Development</h2>
-          <ul>
-            <li>Front-end Development</li>
-            <li>Vue/Nuxt</li>
-            <li>Accessibility</li>
-            <li>Web3</li>
-          </ul>
-          <p>I have decades of experience building software used by hundreds of thousands of users around the world. I focus on creating fast, easy-to-use, and accessible frontends in modern JavaScript frameworks.</p>
-        </section>
-        <!-- <section class="single-service analysis">
-          <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5 5V85C5 90.5228 9.47715 95 15 95H95" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <rect x="15" y="59" width="12" height="26" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <rect x="35" y="20" width="12" height="65" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <rect x="55" y="45" width="12" height="40" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            <rect x="75" y="13" width="12" height="72" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-          </svg>
-          <h2>Analysis</h2>
-          <ul>
-            <li>Product Success</li>
-            <li>Team Process</li>
-            <li>User Journey</li>
-            <li>Metrics Definition</li>
-          </ul>
-          <p>An extension of the product cycle is having smart analysis of the product that you have built. This means setting up analytics tools like Hotjar, and monitoring them with an intent to feed that output back into your product cycle. It's critical to iterate based on hard data and not intuition.</p>
-        </section> -->
       </section>
-    </section>
 
-    <section id="Rate">
-      <h2 class="center">My rate is $45/h, and I book up to 160 hours of work monthly.</h2>
-      <!-- <Button text="Add your project" size="default" to="/booking" /> -->
-      <a class="button" href="mailto:ramijames@gmail.com">Add your project</a>
-    </section>
+      <section id="Rate">
+        <h2 class="center">My rate is $45/h, and I book up to 160 hours of work monthly.</h2>
+        <!-- <Button text="Add your project" size="default" to="/booking" /> -->
+        <a class="button" href="mailto:ramijames@gmail.com">Add your project</a>
+      </section>
 
-  </section>
+    </section>
+  </main>
   
   <Footer />
 </template>
@@ -124,6 +131,15 @@ import Button from '/components/Button';
 <style scoped lang="scss">
 
 @import './assets/variables';
+
+#Services {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  perspective: 100rem;
+}
 
 .services-hero {
   padding: $spacing-lg 0 0 0;
@@ -201,8 +217,8 @@ import Button from '/components/Button';
       opacity: 0;
       transition: all 0.35s ease-in-out;
 
-      // There are 35 letters, so let's iterate through them and delay each one
-      @for $i from 1 through 35 {
+      // There are 8 letters, so let's iterate through them and delay each one
+      @for $i from 1 through 8 {
         &:nth-child(#{$i}) {
           animation-delay: $i * 0.025s;
         }
@@ -356,6 +372,93 @@ import Button from '/components/Button';
       color: $purple;
     }
   } 
+}
+
+.mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0,0,0,0.5);
+  z-index: -1;
+  mask-image: linear-gradient( rgba(0,0,0,0) 5%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 80%);
+}
+
+#mesh-gradient.sixteen-elements {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  z-index: -10;
+  align-items: center;
+  justify-content: center;;
+  opacity: 1;
+  position: fixed;
+  pointer-events: none;
+  top:0;
+  left:0;
+  right:0;
+  animation: rotateGroup 120s linear infinite;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+
+  $mesh-colors: ();
+
+  // generate the colors with an HSL model
+  @for $i from 0 through 360 {
+    $hue: ($i - 1) * calc(360 / 60);
+    $color: hsl($hue, 100%, 50%);
+    $mesh-colors: map-merge($mesh-colors, ($i: $color));
+  }
+
+  $i: 0;
+  @each $name, $color in $mesh-colors {
+    $i: $i + 1;
+    .element:nth-child(#{$i}) {
+      // background: linear-gradient(to bottom, rgba($color,0) 55%, rgba($color,.8));
+      border: 1px solid rgba($color, .4);
+      animation-delay: -#{$i * .15}s;
+      width: 10dvw;
+      height: 10dvw;
+      border-radius: 1rem;
+      align-self: center;
+      justify-self: center;
+    }
+  }
+
+  .element {
+    animation: sixteen-elements 10s linear infinite;
+  }
+
+  @keyframes rotateGroup {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes sixteen-elements {
+    0% {
+      opacity:0;
+      transform: scale(0) rotate(0deg);
+    }
+    50% {
+      opacity: 1;
+      transform: scale(1.6) rotate(180deg);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(0) rotate(360deg);
+    }
+  }
 }
 
 </style>
