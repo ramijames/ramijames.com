85e9e496a34672f9dc17777e2c5fc6a14ccc580a
Rami James <ramijames@gmail.com>
2024-11-12 13:19:32 +0200

    Simpler mergedprojects

--- components/MergedProjects.vue
+++ components/MergedProjects.vue
@@ -5,12 +5,13 @@
         v-for="product in products" 
         :key="product.title" 
         :to="product.slug" 
-        :style="{ backgroundColor: product.color, backgroundImage: `url(${product.bg})` }"
+        :style="{ backgroundColor: product.color }"
       >
       <section :class="['info', product.class]">
+        <img :src="product.logo" alt="Logo" class="logo" />
         <h3>{{ product.title }}</h3>
-        <p>View Product</p>
       </section>
+      <img :src="product.image" alt="preview screenshot" class="preview" />
     </nuxt-link>
   </section>
 </template>
@@ -27,6 +28,7 @@ export default {
           title: 'Scatter',
           description: 'Open-source web3 wallet for EOS, Ethereum, and Tron',
           image: '/products/scatter/scatter-thumb.png',
+          logo: '/products/scatter/scatter-logo.png',
           bg: '/homepage/hp-scatter.png',
           status: 'past',
           slug: '/products/scatter',
@@ -38,6 +40,7 @@ export default {
           title: 'Ultra',
           description: 'Tokenized gaming platform',
           image: '/products/ultra/ultra-prime-thumb.png',
+          logo: '/products/ultra/ultra-logo.svg',
           bg: '/homepage/hp-ultra.png',
           status: 'past',
           slug: '/products/ultra',
@@ -49,6 +52,7 @@ export default {
           title: 'Doodledapp',
           description: 'No-code smart contract development and deployment',
           image: '/products/doodledapp/doodledapp-thumb.png',
+          logo: '/products/doodledapp/doodledapp-logo.svg',
           bg: '/homepage/hp-doodledapp.png',
           status: 'past',
           slug: '/products/doodledapp',
@@ -60,6 +64,7 @@ export default {
           title: 'Food For Future',
           description: 'Decentralized data for funding farmers',
           image: '/products/food-for-future/fff-thumb.png',
+          logo: '/products/food-for-future/fff-logo.svg',
           bg: '/homepage/hp-fff.png',
           status: 'past',
           slug: '/products/food-for-future',
@@ -71,6 +76,7 @@ export default {
           title: 'Telos Open Block Explorer',
           description: 'The best way to view transactions and manage your wallets',
           image: '/products/telos/telos-thumb.png',
+          logo: '/products/telos/telos-logo.png',
           bg: '/homepage/hp-telos.png',
           status: 'past',
           slug: '/products/telos-obe',
@@ -78,17 +84,17 @@ export default {
           class: 'telos',
           type: 'Product Case Study'
         },
-        {
-          title: 'Illustrations',
-          description: 'Various illustrations during my journey',
-          image: '/products/illustrations/illustrations-thumb.png',
-          bg: '/homepage/hp-illustrations.jpg',
-          status: 'past',
-          slug: '/products/illustrations',
-          color: '#0899FE',
-          class: 'illustrations',
-          type: 'Skillset Showcase'
-        },          
+        // {
+        //   title: 'Illustrations',
+        //   description: 'Various illustrations during my journey',
+        //   image: '/products/illustrations/illustrations-thumb.png',
+        //   bg: '/homepage/hp-illustrations.jpg',
+        //   status: 'past',
+        //   slug: '/products/illustrations',
+        //   color: '#0899FE',
+        //   class: 'illustrations',
+        //   type: 'Skillset Showcase'
+        // },          
       ]
     }
   },
@@ -113,7 +119,7 @@ export default {
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
   position: relative;
-  gap: $spacing-lg;
+  gap: $spacing-md;
   opacity: 0;
   animation: fadeInUp 0.3s forwards ease-in-out;
   animation-delay: 0.4s;
@@ -125,7 +131,7 @@ export default {
 
   .project {
     width: 100%;
-    height: 60vh;
+    height: 600px;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
@@ -136,46 +142,32 @@ export default {
     opacity: 1;
     transition: all 0.3s ease-in-out;
     padding: 0;
+    overflow: hidden;
+
+    .preview {
+      position: absolute;
+      bottom: -200px;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 1;
+      transition: all 0.3s ease;
+    }
 
     &:hover {
       opacity: 1;
+
+      img.preview {
+        bottom: -100px;
+        opacity: 1;
+      }
 
       .info {
         opacity: 1;
         transform: translateY(0);
       }
 
-      &:after {
-        opacity: 1;
-      }
-    }
-
-    @media screen and (max-width: 768px){
-      align-items: center;
-      justify-content: center;
-      transform: scale(1);
-    }
-
-    &.ultra,
-    &.foodforfuture {
-      background-position: right center;
-    }
-
-    &:after {
-      content: '';
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: $black;
-      mix-blend-mode: color;
-      z-index: 0;
-      opacity: 1;
-      transition: all 0.3s ease-in-out;
-
-      @media screen and (max-width: 768px){
-        background: $black;
+      &:before {
+        background: radial-gradient(circle, rgba(0,0,0,0.1) 0%, rgba(0, 0, 0, 0.25) 100%);
         opacity: 1;
       }
     }
@@ -187,24 +179,29 @@ export default {
       left: 0;
       width: 100%;
       height: 100%;
-      background: linear-gradient(30deg, rgba($black, 0.6) 70%, rgba($black, 0.0) 100%);
+      background: radial-gradient(circle, rgba(0,0,0,0.1) 40%, rgba(0, 0, 0, 0.25) 100%);
       z-index: 1;
-      opacity: 1;
-      transition: all 0.3s ease-in-out;
+      opacity: .6;
+      transition: all 0.3s ease;
     }
 
     .info {
       display: flex;
       flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
+      justify-content: flex-start;
+      align-items: center;
       opacity: 1;
       transition: all 0.3s ease-in-out;
       z-index: 1;
-      background: rgba($black, 0.6);
-      backdrop-filter: blur(10px);
       padding: $spacing-md;
-      width: 50%;
+      width: 100%;
+      height: 100%;
+
+      img.logo {
+        width: 100%;
+        max-width: 100px;
+        margin-top: $spacing-lg;
+      }
 
       @media screen and (max-width: 768px){
         align-items: center;
@@ -224,7 +221,7 @@ export default {
     }
 
     h3 {
-      color: $red;
+      color: $white;
       border: 0;
       line-height: 100%;
       padding: 0;
@@ -233,7 +230,7 @@ export default {
       position: relative;
 
       @media screen and (max-width: 768px){
-        font-size: 10vw;
+        font-size: 6vw;
         text-wrap: balance;
         text-align: center;
 
