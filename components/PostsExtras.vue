eeb94c9e4e5b1ae0629672498598aa39724ace62
Rami James <ramijames@gmail.com>
2025-01-22 15:12:57 +0200

    Removing vewrite from posts

--- components/PostsExtras.vue
+++ components/PostsExtras.vue
@@ -1,11 +1,11 @@
 <template>
   <section id="PostsExtras">
-    <section class="w-content">
+    <!-- <section class="w-content">
       <img src="/vewrite.png" alt="Vewrite - project management for technical writers" />
       <h3>Project Management for Content Writers</h3>
       <p>Vewrite, project management tailored for writing teams, is launching soon. Make sure to sign up for the waitlist so that you can be notified when the product launches.</p>
       <a href="https://vewrite.com/" class="button primary large">Check it out</a>
-    </section>
+    </section> -->
   </section>
 </template>
 
@@ -13,49 +13,49 @@
 
 @import '/assets/variables.scss';
 
-#PostsExtras {
-  background: $white;
-  width: 100%;
-  padding: 0 0 $spacing-lg ;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+// #PostsExtras {
+//   background: $white;
+//   width: 100%;
+//   padding: 0 0 $spacing-lg ;
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
 
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-  }
+//   @media screen and (max-width: 768px) {
+//     flex-direction: column;
+//     align-items: center;
+//   }
 
-  .w-content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 0;
+//   .w-content {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     padding: 0;
 
-    img {
-      width: 100%;
-      border-radius: $br-lg;
-      border: 1px solid $gray;
-      margin: 0 auto $spacing-md;
-    }
+//     img {
+//       width: 100%;
+//       border-radius: $br-lg;
+//       border: 1px solid $gray;
+//       margin: 0 auto $spacing-md;
+//     }
 
-    h3 {
-      margin-top: 0;
-      line-height: 140%;
-      margin-bottom: $spacing-xs;
-      text-align: center;
-    }
+//     h3 {
+//       margin-top: 0;
+//       line-height: 140%;
+//       margin-bottom: $spacing-xs;
+//       text-align: center;
+//     }
 
-    p {
-      font-size: $font-size-sm;
-      margin: 0 auto $spacing-md;
-      text-align: center;
+//     p {
+//       font-size: $font-size-sm;
+//       margin: 0 auto $spacing-md;
+//       text-align: center;
 
-      @media screen and (max-width: 768px) {
-        text-align: center;
-      }
-    }
-  }
-}
+//       @media screen and (max-width: 768px) {
+//         text-align: center;
+//       }
+//     }
+//   }
+// }
 
 </style>
