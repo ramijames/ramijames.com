2b73219630364c6285ee7452ae516d9fa2c3bd43
Rami James <ramijames@gmail.com>
2025-02-13 16:52:27 +0200

    Updating about to include Vewrite

--- pages/about.vue
+++ pages/about.vue
@@ -19,94 +19,17 @@
       <p class="center">My name is Rami James. I design products and implement front-ends in Vue and Nuxt. You can find me on <a href="https://twitter.com/ramijames">Twitter</a>, <a href="https://github.com/ramijames">Github</a>, and <a href="https://www.linkedin.com/in/rami-james/">LinkedIn</a>. I'm always building something. These days I'm focused on building <a href="https://vewrite.com/">Vewrite</a>, a project management tool for developer relations, technical writing teams, and writers.</p>
     </section>
 
-    <!-- <section class="w-three-quarters">
-      <section id="ServicesGrid">
-          <section class="single-service product">
-            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M32.0538 68.2397C32.0538 63.6984 29.5885 54.6157 19.7274 54.6157C7.40091 54.6157 2.21083 68.8884 6.75215 76.6736C11.2935 84.4587 15.186 89 25.5662 89C33.8703 89 58.8692 89 71.6282 89C82.6571 89 91.7398 83.8099 91.7398 74.7273C91.7398 63.6984 84.9256 61.1033 84.9256 61.1033" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M78.7646 66.2934C82.4409 64.9959 88.496 59.676 83.3059 48.7768C78.7646 39.2401 69.6819 40.9917 69.6819 40.9917" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M63.8431 68.8884C57.1392 69.5372 45.4183 74.4678 52.1654 89" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M59.3018 54.6157C56.2742 57.6433 51.1274 65.1256 54.7604 70.8347" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M46.3265 53.967C48.7053 53.3182 53.8522 53.3182 55.4092 58.5083" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M26.215 39.6942C29.0263 34.5041 37.5034 26.5893 48.9216 36.4504C57.3555 30.6116 76.8183 36.4504 67.7356 52.0207" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M28.4834 57.2107C29.9972 55.2645 36.2686 52.0207 41.1364 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M37.2439 45.5331C32.0538 36.4504 10.2555 38.5264 11.2935 57.2107" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M12.2644 25.4215L7.72311 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M89.4669 25.4215L94.0083 17.6364" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M50.5413 20.2314V10.5" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            </svg>
-            <h3>Product</h3>
-            <ul>
-              <li>Concept</li>
-              <li>Vision</li>
-              <li>Roadmapping</li>
-              <li>Leadership</li>
-            </ul>
-            <p>Ensuring that you are building the right things for the right reasons can be a monumental task. I have decades of experience helping teams make smart decisions about their product, ensuring that it matches both business and user needs.</p>
-          </section>
-          <section class="single-service design">
-            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.5914 31.0464C-14.4508 84.3277 52.1508 106.173 62.2743 90.1887C64.9383 82.7293 45.1324 81.6636 53.2164 69.9418C61.3005 58.2199 86.2508 78.4668 92.6446 61.9496C108.564 20.8251 36.9438 -25.0226 10.5914 31.0464Z" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <circle cx="73.9961" cy="48.0965" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <circle cx="55.8805" cy="25.7183" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <circle cx="30.3055" cy="36.3745" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <circle cx="27.1086" cy="65.1465" r="8.52501" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            </svg>
-            <h3>Design</h3>
-            <ul>
-              <li>Wireframing</li>
-              <li>Prototyping</li>
-              <li>Mockups</li>
-              <li>Illustrations</li>
-            </ul>
-            <p>From user experience wireframes and prototypes to high-fidelity mockups, I excel at making simple looking, highly usable interfaces that stand out in our shared competitive market.</p>
-          </section>
-          <section class="single-service development">
-            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="5" y="5" width="90" height="90" rx="10" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M5 24L9.62344 24C12.3362 24 14.5541 21.8369 14.6219 19.125L14.6281 18.875C14.6959 16.1631 16.9138 14 19.6266 14L47.5 14C50.2614 14 52.5 16.2386 52.5 19V19C52.5 21.7614 54.7386 24 57.5 24L95 24" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <path d="M5 86L95 86" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            </svg>
-            <h3>Development</h3>
-            <ul>
-              <li>Front-end Development</li>
-              <li>Vue/Nuxt</li>
-              <li>Accessibility</li>
-              <li>Web3</li>
-            </ul>
-            <p>I have decades of experience building software used by hundreds of thousands of users around the world. I focus on creating fast, easy-to-use, and accessible frontends in modern JavaScript frameworks.</p>
-          </section>
-          <section class="single-service analysis">
-            <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M5 5V85C5 90.5228 9.47715 95 15 95H95" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <rect x="15" y="59" width="12" height="26" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <rect x="35" y="20" width="12" height="65" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <rect x="55" y="45" width="12" height="40" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-              <rect x="75" y="13" width="12" height="72" rx="6" stroke="#333333" stroke-width="3" stroke-linecap="round"/>
-            </svg>
-            <h3>Analysis</h3>
-            <ul>
-              <li>Product Success</li>
-              <li>Team Process</li>
-              <li>User Journey</li>
-              <li>Metrics Definition</li>
-            </ul>
-            <p>An extension of the product cycle is having smart analysis of the product that you have built. This means setting up analytics tools like Hotjar, and monitoring them with an intent to feed that output back into your product cycle. It's critical to iterate based on hard data and not intuition.</p>
-          </section>
-        </section>
-    </section> -->
-
     <section class="w-content">
       <h2>Experience</h2>
       <section class="jobs">  
         <section class="single-job">
-          <p class="job-title">Freelance</p>
-          <p class="job-subtitle">Contractor</p>
-          <p class="job-duration">Present</p>
-          <p class="job-descriptions">Currently I provide a variety of product, development, and devrel services as a solo contractor. If you need those kinds of services, you can email me directly at <a href="mailto:ramijames@gmail.com">ramijames@gmail.com</a>.</p>
-          <p class="job-descriptions">I have:</p>
+          <p class="job-title">Vewrite</p>
+          <p class="job-subtitle">Founder</p>
+          <p class="job-duration">2024 - Present</p>
+          <p class="job-descriptions">Currently I'm the founder, product and development lead at Vewrite.</p>
+          <p class="job-descriptions">My day to day includes</p>
           <ul class="job-descriptions">
-            <li v-for="(description, index) in freelanceContractor" :key="index">
+            <li v-for="(description, index) in vewrite" :key="index">
               {{ description }}
             </li>
           </ul>
@@ -161,11 +84,13 @@ import Button from '/components/Button';
 export default {
   data() {
     return {
-      freelanceContractor :[
-        'Worked with multiple teams to produce usable and accessible products for web and mobile',
-        'Provided key insights to small teams as they developed their strategy, vision, and roadmap',
-        'Helped fine-tune processes in companies as they experienced growing pains',
-        'Implemented products in a variety of modern javascript frameworks like Vue and Nuxt'
+      vewrite :[
+        'Building a project management tool for developer relations, technical writing teams, and writers',
+        'Designing and implementing the product',
+        'Building a community of writers and project managers',
+        'Writing documentation and tutorials for the product',
+        'Building a marketing website for the product',
+        'Team lead for a small team of developers, writers, and marketers'
       ],
       ultraDevReldescriptions: [
         'Grew the developer pool from zero to dozens of active developers',
