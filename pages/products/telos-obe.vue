47be2113027e2813da819904e36b6c3e8fa67e85
Rami James <ramijames@gmail.com>
2026-01-05 10:33:12 +0200

    Telos

--- pages/products/telos-obe.vue
+++ pages/products/telos-obe.vue
@@ -1,16 +1,53 @@
 <template>
 
-  <ProjectsHeader project="Telos OBE" logo="/products/telos/telos-logo.png" />
+  <main class="w-product-page">
+    <MergedProjects vertical="true" />
 
-  <section class="w-full">
-    <img src="/products/telos/telos-mobile.png" alt="Telos OBE Home" class="single-product-image">
-    <img src="/products/telos/telos-obe-home.png" alt="Telos OBE Home" class="single-product-image">
-    <img src="/products/telos/telos-obe-various.png" alt="Telos OBE Various Screens" class="single-product-image">
-  </section>
+    <div>
 
-  <MergedProjectsHeader />
-  <MergedProjects />
-  <Footer />
+      <ProjectsHeader project="Telos OBE" logo="/products/telos/telos-logo.png" />
+
+      <ProjectsOverview
+        title="Telos OBE"
+        tagline="Open source block explorer"
+        overview="I created the UX and designed the Telos Open Block explorer as part of my wider participation in the Telos ecosystem."
+        :services="['Design', 'UX']"
+      />
+
+      <section class="w-full">
+        <section class="content">
+
+          <section class="discuss">
+            <div class="with-line">
+              <h3>Easy to use, feature-filled</h3>
+              <p>The core challenge with the Telos open block explorer design was that it was chock full of features. My previous experience with Scatter made me uniquely qualified to understand the hierarchy and feature sets that were required to make this design really shine.</p>
+            </div>
+            <div class="discuss-images">
+              <img src="/products/telos/telos-obe-home.png" alt="Telos OBE Home" class="single-product-image">
+              <img src="/products/telos/telos-obe-various.png" alt="Telos OBE Various Screens" class="single-product-image">
+            </div>
+          </section>
+
+          <section class="discuss">
+            <div class="with-line">
+              <h3>Mobile reaady</h3>
+              <p>An interested side-effect of designing the block explorer meant that we were able to implement many features for the first time for mobile users.</p>
+            </div>
+            <div class="discuss-images">
+              <img src="/products/telos/telos-mobile.png" alt="Telos OBE Home" class="single-product-image">
+            </div>
+          </section>
+
+
+        </section>
+      </section>
+    </div>
+  
+  </main>
+
+  <MergedProjectsHeader mobile="true" />
+  <MergedProjects mobile="true" />
+
 </template>
 
 <script setup>
