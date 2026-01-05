0a5ffb1ef3d2a03f893b7c00d1fadaccf23b4b23
Rami James <ramijames@gmail.com>
2026-01-05 10:16:38 +0200

    FFF

--- pages/products/climbingmonster.vue
+++ pages/products/climbingmonster.vue
@@ -65,37 +65,6 @@
   
 </template>
 
-
-  <!-- <section class="w-half">
-    <p>The product vision of Climbing Monster was to allow climbing trainers and athelets to build structured workouts that could be tracked, analyzed, and improved. The business model was focused on providing benefit for gyms that would increase loyalty through athletic improvement.</p>
-    <p>I was the Head of Product for the POC. It was my job to produce high-fidelity mockups for the mobile application (iOS), for the web-based training app, for advertising purposes, and the pitch decks that we used to try and get funding.</p>
-  </section> -->
-
-  <!-- <section class="w-full">
-    <img src="/products/climbingmonster/mobile-flow.png" alt="Climbing Monster mobile app for iOS" class="single-product-image">
-  </section>
-
-  <section class="w-full">
-    <section class="product-images three-across">
-      <img src="/products/climbingmonster/cm-createsession-welcome.png" alt="" class="single-product-image">
-      <img src="/products/climbingmonster/cm-createsession-calendar.png" alt="" class="single-product-image">
-      <img src="/products/climbingmonster/cm-createsession-current.png" alt="" class="single-product-image">
-    </section>
-  </section>
-
-  <section class="w-full">
-    <img src="/products/climbingmonster/77c0cd32013425.5605afad2ae28.png" alt="Climbing Monster web interface" class="single-product-image">
-    <img src="/products/climbingmonster/09078932013445.5605af7a1f287.png" alt="Climbing Monster web interface" class="single-product-image">
-  </section>
-
-  <section class="w-full">
-    <img src="/products/climbingmonster/cm-brand.png" alt="Climbing Monster Brand" class="single-product-image">
-  </section>
-
-  <MergedProjectsHeader />
-  <MergedProjects />
-  <Footer /> -->
-
 <script setup>
 
 import { onMounted, onUnmounted, ref } from 'vue';
--- pages/products/food-for-future.vue
+++ pages/products/food-for-future.vue
@@ -1,8 +1,52 @@
 <template>
 
+  <main class="w-product-page">
+    <MergedProjects vertical="true" />
+
+    <div>
+
   <ProjectsHeader project="Food For Future" logo="fff-logo.svg" />
 
-  <section class="w-full">
+  <ProjectsOverview
+        title="Food For Future"
+        tagline="Decentralized data for funding farmers"
+        overview="I designed the POC which was presented to the UN."
+        :services="['Design', 'UX', 'Branding']"
+      />
+
+      <section class="w-full">
+        <section class="content">
+
+          <section class="discuss">
+            <div class="with-line">
+              <h3>Mobile access to critical data</h3>
+              <p>I created the initial designs for the Food for Future mobile application which allowed potential funders to access critical data on the state of farms in deep rural areas.</p>
+            </div>
+            <div class="discuss-images">
+              <section class="product-images three-across">
+                <img src="/products/food-for-future/fff-global-short.png" alt="" class="single-product-image">
+                <img src="/products/food-for-future/fff-farms-explore.png" alt="" class="single-product-image">
+                <img src="/products/food-for-future/fff-farms-single.png" alt="" class="single-product-image">
+              </section>
+              <section class="product-images two-across">
+                <img src="/products/food-for-future/fff-field-single.png" alt="" class="single-product-image">
+                <img src="/products/food-for-future/fff-menu-open.png" alt="" class="single-product-image">
+              </section>
+            </div>
+          </section>
+
+          <section class="discuss">
+            <div class="with-line">
+              <h3>Branding</h3>
+              <p>As part of the pitch process, I designed a concept brand, palette, and icon set. These were intended to present a fresh, modern take on dry data.</p>
+            </div>
+            <div class="discuss-images">
+              <img src="/products/food-for-future/fff-brand.png" alt="Food for Future brand" class="single-product-image">
+              <img src="/products/food-for-future/fff-icons.png" alt="Food for Future icons" class="single-product-image">
+            </div>
+          </section>
+
+  <!-- <section class="w-full">
     <section class="product-images three-across">
       <img src="/products/food-for-future/fff-global-short.png" alt="" class="single-product-image">
       <img src="/products/food-for-future/fff-farms-explore.png" alt="" class="single-product-image">
@@ -20,11 +64,18 @@
   <section class="w-full">
     <img src="/products/food-for-future/fff-brand.png" alt="Food for Future brand" class="single-product-image">
     <img src="/products/food-for-future/fff-icons.png" alt="Food for Future icons" class="single-product-image">
-  </section>
+  </section> -->
 
-  <MergedProjectsHeader />
-  <MergedProjects />
-  <Footer />
+
+        </section>
+      </section>
+    </div>
+  
+  </main>
+
+  <MergedProjectsHeader mobile="true" />
+  <MergedProjects mobile="true" />
+  
 </template>
 
 <script setup>
