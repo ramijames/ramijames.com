dad201bdd3c705853813ed9e70bd1fabbd8b1656
Rami James <ramijames@gmail.com>
2026-01-22 08:04:20 +0200

    Better MaxNav

--- components/MaxNav.vue
+++ components/MaxNav.vue
@@ -11,7 +11,6 @@
           to="/products"
         >
         <svg viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="1" y="1" width="56" height="56" rx="5" stroke="white" stroke-width="2"/>
           <path d="M28.4706 21.5294L21 29L29 37" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M22 29H38" stroke="white" stroke-width="2" stroke-linecap="round"/>
         </svg>
@@ -26,7 +25,6 @@
           to="/thoughts"
         >
         <svg viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="1" y="1" width="56" height="56" rx="5" stroke="white" stroke-width="2"/>
           <path d="M28.4706 21.5294L21 29L29 37" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M22 29H38" stroke="white" stroke-width="2" stroke-linecap="round"/>
         </svg>
@@ -35,7 +33,6 @@
       <!-- Home Logo -->
       <nuxt-link to="/" class="logo-link">
         <svg viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="1" y="1" width="56" height="56" rx="5" stroke="white" stroke-width="2"/>
           <path d="M41.6492 27.2521C46.0072 27.2521 50.4345 23.8378 51.5379 19.626C52.6412 15.4143 50.0028 12 45.6448 12C43.3509 12 40.5896 12.8841 38.235 13.4079C34.9228 14.1448 32.3003 14.8457 30.6837 15.3147C29.9858 15.5171 29.427 15.0553 29.5987 14.4L29.9115 13.2058C30.0718 12.594 29.5934 12.1457 28.9404 12.2958L10.0807 16.631C9.62206 16.7364 9.22203 17.1113 9.10947 17.5409L9.03517 17.8245C8.95239 18.1405 9.05119 18.4322 9.29392 18.589C9.50465 18.725 9.7979 18.7424 10.0871 18.636L11.4073 18.1506C11.8618 17.9835 12.3048 18.1079 12.482 18.4525L12.5345 18.5545C12.618 18.717 12.6343 18.9146 12.5811 19.1177L5.78471 45.0609C5.62511 45.6702 6.09891 46.1179 6.74924 45.9724L21.451 42.68C21.912 42.5768 22.3157 42.2006 22.4288 41.7686L28.5036 18.5802C28.6141 18.1583 29.0022 17.7883 29.4526 17.6757L36.3585 15.9485C36.8046 15.837 37.1905 16.0371 37.2662 16.419C37.2738 16.458 37.2646 16.5025 37.2365 16.5415C36.5574 17.4846 36.0438 18.5282 35.7562 19.626C34.6529 23.8378 37.2913 27.2521 41.6492 27.2521Z" stroke="white" stroke-width="2"/>
         </svg>
 
@@ -43,7 +40,7 @@
     </section>
 
     <section class="nav-right-section">
-      <button class="start-project" @click="openContactModal" v-show="!footerCtaVisible">Start Project</button>
+      <button class="start-project white" @click="openContactModal" v-show="!footerCtaVisible">Start Project</button>
       <div
         :class="['menu-switch', state.mobileMenuOpen ? 'open' : '' ]"
         @click="toggleMenu"
@@ -52,7 +49,6 @@
         v-if="!state.mobileMenuOpen"
       >
         <svg viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg" class="menu-icon">
-          <rect x="1" y="1" width="56" height="56" rx="5" stroke="white" stroke-width="2"/>
           <path d="M17 19H41" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M17 29H33" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M17 39H41" stroke="white" stroke-width="2" stroke-linecap="round"/>
@@ -66,7 +62,6 @@
         v-else
       >
         <svg viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg" class="close-icon">
-          <rect x="1" y="1" width="56" height="56" rx="5" stroke="white" stroke-width="2"/>
           <path d="M21 21L37.9706 37.9706" stroke="white" stroke-width="2" stroke-linecap="round"/>
           <path d="M37.9707 21L21.0001 37.9706" stroke="white" stroke-width="2" stroke-linecap="round"/>
         </svg>
@@ -236,15 +231,16 @@ const isHome = computed(() => {
     align-items: center;
     justify-content: center;
     gap: 0;
+    background: $white;
+    border-radius: $br-md;
+    corner-shape: squircle(8%);
+    padding: 0 $spacing-sm;
 
     .go-back {
       display: block;
       width: 58px;
       height: 58px;
-
-      @media screen and (max-width: 1000px){
-        transform: scale(0.8);
-      }
+      margin-left: -$spacing-sm;
 
       &:hover {
         transform: scale(1.05);
@@ -301,35 +297,20 @@ const isHome = computed(() => {
     flex-direction: row;
     align-items: center;
     gap: $spacing-md;
+    background: $white;
+    border-radius: $br-md;
+    corner-shape: squircle(8%);
+    padding: 0 $spacing-sm;
 
     .start-project {
-      font-size: $font-size-lg;
-      font-weight: 600;
       font-family: $font-family-main;
-      color: rgba($black, 1);
       cursor: pointer;
       transition: color 0.3s ease-in-out;
       text-decoration: none;
-      background: white;
       border: none;
-      padding: 0 $spacing-sm !important;
-      height: 54px;
-      border-radius: 5px;
 
       @media screen and (max-width: 1000px){
         display: none;
-      }
-
-      &:after {
-        content: '';
-        display: block;
-        width: 100%;
-        height: 4px;
-        background: rgba($black, 1);
-        margin-top: 4px;
-        transform: scaleX(0);
-        transition: transform 0.3s ease-in-out;
-        transform-origin: left;
       }
 
       &:hover {
@@ -348,10 +329,6 @@ const isHome = computed(() => {
     height: 56px;
     cursor: pointer;
     position: relative;
-
-    @media screen and (max-width: 1000px){
-      transform: scale(0.8);
-    }
 
     &:hover {
       transform: scale(1.05);
