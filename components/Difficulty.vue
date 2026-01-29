68542d55cb8aa45bf8f57b47474bfd65d35e2edc
Rami James <ramijames@gmail.com>
2026-01-29 14:09:15 +0200

    Updated nav

---
+++ components/Difficulty.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="difficulty-wrapper">
+    <div :class="['dot lit']"></div>
+    <div :class="['dot', value == 'intermediate' ? 'lit' : '']"></div>
+    <div :class="['dot', value == 'advanced' ? 'lit' : '']"></div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  value: String
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+.dark {
+  .difficulty-wrapper {
+
+    .dot {
+      background: rgba($white, 0.2);
+
+      &.lit {
+        background: rgba($white, 1);
+      }
+    }
+  }
+}
+
+.difficulty-wrapper {
+  padding: $spacing-xs 0;
+  display: flex;
+  flex-direction: row;
+  gap: 3px;
+
+  .dot {
+    width: 6px;
+    height: 6px;
+    border-radius: 3px;
+    background: rgba($black, 0.2);
+
+    &.lit {
+      background: rgba($black, 1);
+    }
+  }
+}
+
+</style>
--- components/LearnThreejsNav.vue
+++ components/LearnThreejsNav.vue
@@ -7,96 +7,60 @@
   <nav :class="['learnthreejs', innerNav ? 'innerNav' : '', !isOpen ? 'hidden' : '']">
     <section :class="['threejs-nav', innerNav ? 'innerNav' : '', innerNav && !isOpen ? 'collapsed' : '']">
       <div :class="['nav-content', innerNav && !isOpen ? 'hidden' : '']">
-      <h3>
-        Foundational Three.js
-      </h3>
+      
+        <h3>Foundational Three.js</h3>
 
-      <ol type="I">
-        <li>The 3D Web Revolution</li>
-          <ol type="I">
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/why-threejs">Why Three.js?</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/setting-up-your-dev-environment">A modern dev environment</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/hello-cube">"Hello Cube"</nuxt-link></li>
-          </ol>
-        <li>Scene, Camera, and Renderer</li>
-          <ol type="I">
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/the-scene">The Scene</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/cameras">Cameras</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/the-renderer">The Renderer</nuxt-link></li>
-          </ol>
-        <li>The Scene Graph and Transformations</li>
-          <ol type="I">
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/the-scene-graph">The Scene Graph</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/understanding-parent-child-relationships">Understanding relationships</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/position-rotation-and-scale">Position, Rotation, and Scale</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/foundational-threejs/the-animation-loop">The Animation Loop</nuxt-link></li>
-          </ol>
-      </ol>
+          <!-- <h4>The 3D Web Revolution</h4> -->
+          <nuxt-link to="/learn-threejs/foundational-threejs/why-threejs"><Difficulty value="easy" /><span>Why Three.js?</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/setting-up-your-dev-environment"><Difficulty value="easy" /><span>A modern dev environment</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/hello-cube"><Difficulty value="easy" /><span>"Hello Cube"</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/the-scene"><Difficulty value="easy" /><span>The Scene</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/cameras"><Difficulty value="easy" /><span>Cameras</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/the-renderer"><Difficulty value="easy" /><span>The Renderer</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/the-scene-graph"><Difficulty value="easy" /><span>The Scene Graph</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/understanding-parent-child-relationships"><Difficulty value="easy" /><span>Understanding relationships</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/position-rotation-and-scale"><Difficulty value="easy" /><span>Position, Rotation, and Scale</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/foundational-threejs/the-animation-loop"><Difficulty value="easy" /><span>The Animation Loop</span></nuxt-link>
 
-      <h3>
-        The Building Blocks
-      </h3>
+        <h3>The Building Blocks</h3>
 
-      <ol type="I">
-        <li>Geometry</li>
-          <ol type="I">
-            <li><nuxt-link to="/learn-threejs/building-blocks/buffer-geometry">BufferGeometry</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/standard-primitives">Standard primitives</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/custom-geometries">Custom geometries and vertices</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/instanced-mesh">Instanced Mesh</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/procedural-terrain">Procedural terrain</nuxt-link></li>
-          </ol>
-        <li>Materials and Textures</li>
-          <ol type="I">
-            <li><nuxt-link to="/learn-threejs/building-blocks/the-spectrum-of-materials">The spectrum of materials</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/physically-based-rendering">Physically Based Rendering</nuxt-link></li>
-            <li><nuxt-link to="/learn-threejs/building-blocks/advanced-mapping-concepts">Advanced Mapping Concepts</nuxt-link></li>
-          </ol>
-        <li>Lighting</li>
-          <ol type="I">
-            <li>Types of Lighting</li>
-            <li>Shadows</li>
-            <li>Environment Maps</li>
-          </ol>
-      </ol>
+          <nuxt-link to="/learn-threejs/building-blocks/buffer-geometry"><Difficulty value="easy" /><span>BufferGeometry</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/standard-primitives"><Difficulty value="easy" /><span>Standard primitives</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/custom-geometries"><Difficulty value="easy" /><span>Custom geometries and vertices</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/instanced-mesh"><Difficulty value="easy" /><span>Instanced Mesh</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/procedural-terrain"><Difficulty value="easy" /><span>Procedural terrain</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/the-spectrum-of-materials"><Difficulty value="easy" /><span>The spectrum of materials</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/physically-based-rendering"><Difficulty value="easy" /><span>Physically Based Rendering</span></nuxt-link>
+          <nuxt-link to="/learn-threejs/building-blocks/advanced-mapping-concepts"><Difficulty value="intermediate" /><span>Advanced Mapping Concepts</span></nuxt-link>
+          <!-- 
+          <nuxt-link to="">Types of Lighting</nuxt-link>
+          <nuxt-link to="">Shadows</nuxt-link>
+          <nuxt-link to="">Environment Maps</nuxt-link>
 
-      <h3>Working with Assets</h3>
-        <ol type="I">
-          <li>Loading 3D Models</li>
-            <ol type="I">
-              <li>Asset standards</li>
-              <li>Using GLTFLoader</li>
-              <li>Traversing models</li>
-            </ol>
-        </ol>
+        <h3>Working with Assets</h3>
+
+          <nuxt-link to="">Asset standards</nuxt-link>
+          <nuxt-link to="">Using GLTFLoader</nuxt-link>
+          <nuxt-link to="">Traversing models</nuxt-link>
 
       <h3>Interaction & Raycasting</h3>
-        <ol type="I">
-          <li>Clicking on 3D objects</li>
-          <li>Hover states and UI overlays</li>
-          <li>Basic 3D physics integrations</li>
-        </ol>
+
+          <nuxt-link to="">Clicking on 3D objects</nuxt-link>
+          <nuxt-link to="">Hover states and UI overlays</nuxt-link>
+          <nuxt-link to="">Basic 3D physics integrations</nuxt-link>
 
       <h3>Advanced Visuals</h3>
-        <ol type="I">
-          <li>Shaders – The Secret Sauce</li>
-            <ol type="I">
-              <li>Introduction to GLSL</li>
-              <li>ShaderMaterial vs. RawShaderMaterial</li>
-              <li>Prodedural Shaders</li>
-            </ol>
-          <li>Particles & Post-Processing</li>
-            <ol type="I">
-              <li>Using Points for stars, rain, and fire</li>
-              <li>EffectComposer: Adding Bloom, Depth of Field, and Film Grain</li>
-            </ol>
-          <li>Performance & Optimization</li>
-            <ol type="I">
-              <li>Monitoring performance</li>
-              <li>Draw calls, InstancedMesh, and Texture compression</li>
-              <li>Preventing memory leaks</li>
-            </ol>
-        </ol>
+          <nuxt-link to="">Introduction to GLSL</nuxt-link>
+          <nuxt-link to="">ShaderMaterial vs. RawShaderMaterial</nuxt-link>
+          <nuxt-link to="">Prodedural Shaders</nuxt-link>
+
+          <nuxt-link to="">Using Points for stars, rain, and fire</nuxt-link>
+          <nuxt-link to="">EffectComposer: Adding Bloom, Depth of Field, and Film Grain</nuxt-link>
+
+          <nuxt-link to="">Monitoring performance</nuxt-link>
+          <nuxt-link to="">Draw calls, InstancedMesh, and Texture compression</nuxt-link>
+          <nuxt-link to="">Preventing memory leaks</nuxt-link> 
+          -->
       </div>
     </section>
   </nav>
@@ -132,12 +96,30 @@ onMounted(() => {
     }
 
   }
+
+  .threejs-nav {
+    &.innerNav {
+
+      a {
+        color: $white;
+        
+        &.router-link-exact-active,
+        &:hover {
+          background: rgba($white, 0.1);
+          opacity: 1;
+          font-weight: bold;
+          color: $white;
+        }
+      }
+
+    }
+  }
 }
 
 .learnthreejs {
 
   &.innerNav {
-    width: 300px;
+    width: 400px;
     overflow: auto;
     height: 100vh;
     background: rgba($white, 0.8);
@@ -173,7 +155,7 @@ onMounted(() => {
   position: fixed;
   left: $spacing-sm;
   bottom: $spacing-xs;
-  width: 250px;
+  border: 1px solid rgba($white, 0.2);
   z-index: 3;
 
   @media screen and (max-width: 768px) {
@@ -210,52 +192,40 @@ onMounted(() => {
   &.innerNav {
 
     h3 {
-      font-size: $font-size-md;
+      font-size: calc($font-size-md * 1.2);
       padding-bottom: $spacing-xs;
-      text-transform: uppercase;
-      font-weight: bold;
+      margin: $spacing-md 0 $spacing-xs;
+      line-height: 100%;
 
       &:first-of-type {
         margin-top: 0;
       }
     }
 
-    ol {
+    a {
       font-size: $font-size-sm;
-      opacity: 1;
-      margin: $spacing-xs 0 $spacing-xs $spacing-sm;
+      display: block;
+      text-decoration: none;
+      padding: $spacing-xxs $spacing-sm;
+      border-radius: $br-sm;
+      color: $black;
+      opacity: 0.6;
+      display: flex;
+      flex-direction: row;
+      gap: $spacing-sm;
+      align-content: center;
+      margin-bottom: 1px;
 
-      li {
-        font-size: $font-size-sm;
-        opacity: 1;
-
-        a {
-          text-decoration: none;
-          width: 100%;
-          border-radius: $br-xs;
-        }
-
-        ol {
-          font-size: $font-size-sm;
-          opacity: 1;
-          margin: $spacing-sm 0 $spacing-sm $spacing-sm;
-
-          li {
-            font-size: $font-size-sm;
-            opacity: 1;
-
-            a {
-              text-decoration: none;
-              width: 100%;
-              border-radius: $br-xs;
-            }
-          }
-        }
+      span {
+        align-self: center;
       }
-    }
-
-    a.router-link-exact-active {
-      font-weight: bold;
+      
+      &.router-link-exact-active,
+      &:hover {
+        background: rgba($black, 0.1);
+        opacity: 1;
+        font-weight: bold;
+      }
     }
 
   }
